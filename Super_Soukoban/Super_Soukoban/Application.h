7a2d864cba7675ffe04d4edc0df01f1f61d7ef5b
Dani-24 <dani.toledo.23@hotmail.com>
2021-04-16 22:33:39 +0200

    game

---
+++ Super_Soukoban/Super_Soukoban/Application.h
@@ -0,0 +1,28 @@
+#ifndef __APPLICATION_H__
+#define __APPLICATION_H__
+
+#include "Globals.h"
+
+#define NUM_MODULES 10
+
+class Module;
+class ModuleAudio;
+class ModuleBox;
+class ModuleCollisions;
+class ModuleTextures;
+class ModuleInput;
+class ModulePlayer;
+class ModuleRender;
+//class ModuleScene;
+//class ModuleParticles;
+class ModuleWindow;
+
+class Application
+{
+
+public:
+
+
+}
+
+#endif // __APPLICATION_H__
---
+++ Super_Soukoban/Super_Soukoban/Globals.h
@@ -0,0 +1,33 @@
+#ifndef __GLOBALS_H__
+#define __GLOBALS_H__
+
+#include <Windows.h>
+
+// mensajes de info debug LOG:
+#define LOG(format, ...) log(__FILE__, __LINE__, format, __VA_ARGS__);
+void log(const char file[], int line, const char* format, ...);
+
+#define EXIT_FAILURE 1
+#define EXIT_SUCCESS 0
+
+enum class update_status
+{
+	UPDATE_CONTINUE = 1,
+	UPDATE_STOP,
+	UPDATE_ERROR
+};
+
+typedef unsigned int uint; //abreviacion basicamente
+
+// Configuracion de la pantalla 
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+#define SCREEN_SIZE 3
+#define WIN_FULLSCREEN 0
+#define WIN_FULLSCREEN_DESKTOP 0
+#define WIN_BORDERLESS 0
+#define WIN_RESIZABLE 0
+#define VSYNC 1
+
+#endif // __GLOBALS_H__
