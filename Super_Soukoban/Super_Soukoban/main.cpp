8665b17b72c3643cf671b38cbd9cb310a8e8eb85
Dani-24 <dani.toledo.23@hotmail.com>
2021-04-16 21:56:34 +0200

    Base

---
+++ Super_Soukoban/Super_Soukoban/main.cpp
@@ -0,0 +1,15 @@
+#include "External_Libraries/SDL/Include/SDL.h" // abreviar si se puede
+#include "Application.h"
+#include "MemLeaks.h"
+
+enum class main_states {
+	MAIN_CREATION,
+	MAIN_START,
+	MAIN_UPDATE,
+	MAIN_FINISH,
+	MAIN_EXIT
+};
+
+int main(int argc, char* argv[]) {
+
+}
