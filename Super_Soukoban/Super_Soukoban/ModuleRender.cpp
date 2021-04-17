e24449eca8a2973e5bf536e88401e6d22af77d68
Dani-24 <dani.toledo.23@hotmail.com>
2021-04-17 20:26:23 +0200

    game

--- Super_Soukoban/Super_Soukoban/ModuleRender.cpp
+++ Super_Soukoban/Super_Soukoban/ModuleRender.cpp
@@ -37,3 +37,29 @@ update_status ModuleRender::PreUpdate() {
 	return update_status::UPDATE_CONTINUE;
 }
 
+update_status ModuleRender::Update() {
+	
+	// Aquí van movimientos manuales de la camara (Soukoban no tiene)
+
+	return update_status::UPDATE_CONTINUE;
+}
+
+update_status ModuleRender::PostUpdate() {
+	
+	SDL_RenderPresent(renderer);
+
+	return update_status::UPDATE_CONTINUE;
+}
+
+bool ModuleRender::CleanUp() {
+	LOG("Destroing renderer -u.u-");
+
+	if (renderer != nullptr) {
+		SDL_DestroyRenderer(renderer);
+	}
+	return true;
+}
+
+bool ModuleRender::Blit(SDL_Texture* texture,int x,int y, const SDL_Rect* section,float speed) {
+	bool ret = true;
+}
