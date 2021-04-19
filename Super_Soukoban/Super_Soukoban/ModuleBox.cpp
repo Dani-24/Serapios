48d77cb188aa232e5dfc1ae3108dc8bd5728d93a
Dani-24 <dani.toledo.23@hotmail.com>
2021-04-19 23:36:27 +0200

    :D

--- Super_Soukoban/Super_Soukoban/ModuleBox.cpp
+++ Super_Soukoban/Super_Soukoban/ModuleBox.cpp
@@ -6,8 +6,70 @@
 #include "ModuleTextures.h"
 
 
-ModuleEnemies::ModuleEnemies()
+ModuleBox::ModuleBox()
 {
-	for (uint i = 0; i < BOX_MAX; ++i)
+	for (uint i = 0; i < MAX_BOXES; i++)
 		boxes[i] = nullptr;
+}
+ModuleBox::~ModuleBox()
+{
+}
+
+bool ModuleBox::Start() {
+	
+	//texture = App->textures->Load("");		Poner archivo png de la caja
+	
+	return true;
+}
+
+update_status ModuleBox::Update() {
+
+	return update_status::UPDATE_CONTINUE;
+}
+
+update_status ModuleBox::PostUpdate() {
+	for (uint i = 0; i < MAX_BOXES; ++i)
+	{
+		/*if (boxes[i] != nullptr) {
+			boxes[i]->Draw();
+		}*/
+	}
+	return update_status::UPDATE_CONTINUE;
+}
+
+bool ModuleBox::CleanUp() {
+	LOG("Deleting boxes");
+	for (uint i = 0; i < MAX_BOXES; ++i)
+	{
+		if (boxes[i] != nullptr)
+		{
+			delete boxes[i];
+			boxes[i] = nullptr;
+		}
+	}
+
+	return true;
+}
+
+bool ModuleBox::AddBox(int x, int y) {
+	bool ret = false;
+
+	// añadir cajas (to do)
+
+	return ret;
+}
+
+void ModuleBox::OnCollision(Collider* c1, Collider* c2)
+{
+	//for (uint i = 0; i < MAX_BOXES; ++i)
+	//{
+	//	if (boxes[i] != nullptr && boxes[i]->GetCollider() == c1)
+	//	{
+	//		boxes[i]->OnCollision(c2); //Notify the enemy of a collision
+
+	//		delete boxes[i];
+	//		boxes[i] = nullptr;
+	//		break;
+	//	}
+	//}
 }
