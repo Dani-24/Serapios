46e3b20ada803abfa5ab8364d516a31ddc7ac496
Xavierlm11 <79161102+Xavierlm11@users.noreply.github.com>
2021-04-23 11:07:42 +0200

    Tiles

--- Super_Soukoban/Super_Soukoban/ModuleScene.cpp
+++ Super_Soukoban/Super_Soukoban/ModuleScene.cpp
@@ -5,6 +5,18 @@
 #include "ModuleAudio.h"
 #include "ModuleCollisions.h"
 #include "ModuleBox.h"
+int Scene1[10][16] = {
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+};
 
 ModuleScene::ModuleScene()
 {
@@ -24,8 +36,17 @@ bool ModuleScene::Start()
 
 	bgTexture = App->textures->Load("Assets/captura54.png");
 
+	ground = App->textures->Load("Assets/ground.png");
+	wall = App->textures->Load("Assets/wall.png");
+	s_wall = App->textures->Load("Assets/s_wall.png");
+	floor = App->textures->Load("Assets/floor.png");
+	point = App->textures->Load("Assets/point.png");
+	
+
 	App->audio->PlayMusic("Assets/stage1.ogg", 1.0f);
 
+
+	Update(Scene1);
 	
 	// Ejemplo Collider:
 	// App->collisions->AddCollider({ 0, 224, 3930, 16 }, Collider::Type::WALL);
@@ -36,11 +57,17 @@ bool ModuleScene::Start()
 	return ret;
 }
 
-update_status ModuleScene::Update()
+update_status ModuleScene::Update(int arr[10][16])
 {
 	// Mover la camara (no necesario para el soukoban :D)
 	//App->render->camera.x += 0;
-
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int j = 0; j < 16; ++j)
+		{
+			map[i][j] == arr[i][j];
+		}
+	}
 	return update_status::UPDATE_CONTINUE;
 }
 
@@ -49,6 +76,34 @@ update_status ModuleScene::PostUpdate()
 {
 	// Aqui se dibuja el fondo
 	App->render->Blit(bgTexture, 0, 0, NULL);
+
+	int type = 0;
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int j = 0; j < 16; ++j)
+		{
+			type=map[i][j];
+			switch (type)
+			{
+			case 0:
+				App->render->Blit(ground, i*24, j*24, NULL);
+				break;
+
+			case 1:
+				App->render->Blit(wall, i * 24, j * 24, NULL);
+				break;
+			case 2:
+				App->render->Blit(s_wall, i * 24, j * 24, NULL);
+				break;
+			case 3:
+				App->render->Blit(floor, i * 24, j * 24, NULL);
+				break;
+			case 4:
+				App->render->Blit(point, i * 24, j * 24, NULL);
+				break;
+			}
+		}
+	}
 
 	return update_status::UPDATE_CONTINUE;
 }
--- Super_Soukoban/Super_Soukoban/ModuleScene.h
+++ Super_Soukoban/Super_Soukoban/ModuleScene.h
@@ -16,17 +16,25 @@ public:
 	bool Start() override;
 
 	// Updates the scene's background animations
-	update_status Update() override;
-
+	update_status Update(int arr[10][16]) ;
+	//update_status Update() override;
 	// Renderiza
 	update_status PostUpdate() override;
 
 public:
 
 	// Con lo de abajo creas sprites para el fondo
-
+	
 	SDL_Texture* bgTexture = nullptr;
 
+private:
+	SDL_Texture* ground = nullptr;
+	SDL_Texture* wall = nullptr;
+	SDL_Texture* s_wall = nullptr;
+	SDL_Texture* floor = nullptr;
+	SDL_Texture* point = nullptr;
+
+	int map[10][16];
 };
 
 #endif // __MODULE_SCENE_H__
