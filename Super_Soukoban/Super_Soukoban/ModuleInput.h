36b27e2cb0588eeb321622e28d088b6e7d386fe5
laiapachon <79161109+laiapachon@users.noreply.github.com>
2021-05-25 19:22:42 +0200

    INPUT

--- Super_Soukoban/Super_Soukoban/ModuleInput.h
+++ Super_Soukoban/Super_Soukoban/ModuleInput.h
@@ -5,7 +5,15 @@
 #include "Module.h"
 #include "Globals.h"
 
+#include "External_Libraries/SDL/include/SDL_scancode.h"
+
 #define MAX_KEYS 256
+
+#define MAX_PADS 4
+
+struct _SDL_GameController;
+struct _SDL_Haptic;
+
 
 enum KEY_STATE
 {
@@ -13,6 +21,27 @@ enum KEY_STATE
     KEY_DOWN,
     KEY_REPEAT,
     KEY_UP
+};
+
+
+struct GamePad
+{
+	// Input data
+	bool start, back, guide;
+	bool x, y, a, b, l1, r1, l3, r3;
+	bool up, down, left, right;
+	float l2, r2;
+	float left_x, left_y, right_x, right_y, left_dz, right_dz;
+
+	// Controller data
+	bool enabled;
+	int index;
+	_SDL_GameController* controller;
+	_SDL_Haptic* haptic;
+
+	// Rumble controller
+	int rumble_countdown;
+	float rumble_strength;
 };
 
 class ModuleInput :public Module {
@@ -24,8 +53,20 @@ public:
     bool Init() override;
     update_status PreUpdate() override;
     bool CleanUp() override;
+	//ACTIVE gampad when is connected
+	void HandleDeviceConnection(int index);
+	// DESACTIVE gampad when is disconnected
+	void HandleDeviceRemoval(int index);
+	//iterates through all active gamepads
+	void UpdateGamepadsInput();
+
+	bool ShakeController(int id, int duration, float strenght = 0.5f);
+	const char* GetControllerName(int id)const;
+
 public:
     KEY_STATE keys[MAX_KEYS] = { KEY_IDLE };
+	//array for detected pads
+	GamePad pads[MAX_PADS];
 };
 
 #endif // __MODULE_INPUT_H_
